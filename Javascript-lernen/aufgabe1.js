ae8d09fe9ed696f12ec610e4f4675b0fb97fba22
Marco-Maier <72390428+Marco-Maier@users.noreply.github.com>
2020-12-23 18:14:30 +0100

    Add files via upload

---
+++ Javascript-lernen/aufgabe1.js
@@ -0,0 +1,6 @@
+const input = prompt ("Wie alt bist du?");
+console.log (input);
+console.log (typeof input);
+const ganzzahl = parseInt (input);
+console.log(typeof ganzzahl);
+alert (ganzzahl);
---
+++ Javascript-lernen/tutorial.js
@@ -0,0 +1,32 @@
+let playerName; //let playerName = "Max Mustermann", Score = "100";
+playerName = "Max Musterman";
+console.log( typeof playerName);
+playerName = "Lol"
+console.log(playerName);
+
+
+let Name = "Max lol";
+const isMarried = false ; address = undefined
+const brain = null
+console.log(Name);
+console.log(isMarried);
+console.log(typeof address);
+console.log(typeof brain);
+
+Name = 21;
+
+console.log(Name);
+const age = 23;
+const ageAsString = age.toString();
+console.log(age);
+console.log(ageAsString)
+console.log(typeof age)
+console.log(typeof ageAsString)
+
+
+const AgeAsString = "21.2"
+const Age = parseFloat (AgeAsString)
+console.log(Age);
+console.log(AgeAsString)
+console.log(typeof Age)
+console.log(typeof AgeAsString)
