1e0a1e54644bee5db569c19e649ab11c933fab68
Marco-Maier <72390428+Marco-Maier@users.noreply.github.com>
2021-01-18 13:10:51 +0100

    Delete aufgabe1.js

--- Javascript-lernen/aufgabe1.js
+++
@@ -1,6 +0,0 @@
-const input = prompt ("Wie alt bist du?");//pop up am anfang 
-console.log (input);
-console.log (typeof input);
-const ganzzahl = parseInt (input);//umwandeln in eine Zahl
-console.log(typeof ganzzahl);
-alert (ganzzahl);
